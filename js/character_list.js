0561fabcf64d241158a25427c8bb8cfd17a9dcac
Umpoy <iumpoy@yahoo.com>
2018-07-28 18:56:08 -0700

    moved characters to own js file

---
+++ js/character_list.js
@@ -0,0 +1,29 @@
+const spiderman = {
+	name: "Spider-Man",
+	intel: 110,
+	str: 55,
+	speed: 70,
+	durability: 75,
+	power: 75,
+	combat: 95
+}
+
+const ironman = {
+	name: "Iron Man",
+	intel: 100,
+	str: 85,
+	speed: 60,
+	durability: 85,
+	power: 100,
+	combat: 65
+}
+
+const captain = {
+	name: "Captain America",
+	intel: 85,
+	str: 20,
+	speed: 40,
+	durability: 55,
+	power: 60,
+	combat: 100
+}
--- js/main.js
+++ js/main.js
@@ -2,36 +2,6 @@ $(document).ready(intializeApp);
 
 let game;
 
-const spiderman = {
-	name: "Spider-Man",
-	intel: 110,
-	str: 55,
-	speed: 70,
-	durability: 75,
-	power: 75,
-	combat: 95
-}
-
-const ironman = {
-	name: "Iron Man",
-	intel: 100,
-	str: 85,
-	speed: 60,
-	durability: 85,
-	power: 100,
-	combat: 65
-}
-
-const captain = {
-	name: "Captain America",
-	intel: 85,
-	str: 20,
-	speed: 40,
-	durability: 55,
-	power: 60,
-	combat: 100
-}
-
 function intializeApp() {
 	// game = new Game();
 	var Spiderman = new Character(spiderman);
